5e6ea8fb90084fd2b9a18aa91a3a37a400ddb69f
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2018-01-13 17:43:20 -0200

    emscripten travis test. #13: fixing emscripten cmake problems

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -8,5 +8,11 @@ mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../Engine/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
-emconfigure cmake -DEMSCRIPTEN=1 -DCMAKE_C_COMPILER_ENV_VAR=`which emcc` -DCMAKE_CXX_COMPILER_ENV_VAR=`which em++`  -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/Modules/cmake ../
+echo $EMSCRIPTEN
+echo `which emcc`
+source $DIR/../emsdk-portable/emsdk_env.sh
+echo $EMSCRIPTEN
+echo `which emcc`
+emcc --version
+emconfigure cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/Modules/cmake ../
 emmake make $*
