70f20372fbc26ad57791b8f098c6333b4c1973e3
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2018-01-13 17:53:19 -0200

    emscripten travis test. #13: removed cmake modules path

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -8,11 +8,6 @@ mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../Engine/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
-echo $EMSCRIPTEN
-echo `which emcc`
 source $DIR/../emsdk-portable/emsdk_env.sh
-echo $EMSCRIPTEN
-echo `which emcc`
-emcc --version
-emconfigure cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/Modules/cmake ../
-emmake make $*
+cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
+make $*
