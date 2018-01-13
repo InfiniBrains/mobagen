578a6b1b570cf959da6e0579b07b10bf19bd4db3
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2018-01-13 17:29:25 -0200

    emscripten travis test. #13: fixing emscripten cmake problems

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-EMSCRIPTEN=${EMSCRIPTEN:-/usr/local/opt/emscripten/libexec}
 BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../Engine/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
-cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/Modules/cmake ../
-make $*
+emconfigure cmake -DEMSCRIPTEN=1 -DCMAKE_C_COMPILER_ENV_VAR=`which emcc` -DCMAKE_CXX_COMPILER_ENV_VAR=`which em++`  -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=$EMSCRIPTEN/Modules/cmake ../
+emmake make $*
