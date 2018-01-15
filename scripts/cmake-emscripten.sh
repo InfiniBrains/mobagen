9b95910bf93cf91c1444d662888a3a45bf91d563
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2018-01-15 20:23:46 -0200

    Reduced emscripten build size #13

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -9,5 +9,5 @@ mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../Engine/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
 source $DIR/../emsdk-portable/emsdk_env.sh
-cmake -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
+cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 make $*
