cf8ffb50ce6f0ee67ae3b52b54a7a76d26a100c5
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2018-01-16 01:41:13 -0200

    #13: emscripten build using docker on linux

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -8,6 +8,6 @@ mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
 cp $DIR/../Engine/example/html/index.html $DIR/../bin-emscripten/bin/
 cd $DIR/../bin-emscripten
-source $DIR/../emsdk-portable/emsdk_env.sh
+#source $DIR/../emsdk-portable/emsdk_env.sh
 cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 make $*
