181ba7ccba8d347175bf837b315e083747858cfe
Alexandre Tolstenko <tolstenko@gmail.com>
2018-03-15 10:01:58 -0300

    trigger trabis build

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -11,5 +11,5 @@ cd $DIR/../bin-emscripten
 /usr/bin/cmake -DCMAKE_C_COMPILER=`which emcc` -DCMAKE_CXX_COMPILER=`which em++` -DCMAKE_AR=`which llvm-ar` -DCMAKE_MODULE_PATH=$EMSCRIPTEN/cmake/ -DGDCM_BUILD_DOCBOOK_MANPAGES=0 -DGDCM_DOCUMENTATION=0 -DBUILD_DOC=0 -DTEST_BIG_ENDIAN=0 -DOPJ_BIG_ENDIAN=0 -DCMAKE_STATIC_LIBRARY_SUFFIX=".bc" -DCMAKE_SHARED_LIBRARY_SUFFIX=".bc" -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 /usr/bin/cmake -DCMAKE_C_COMPILER=`which emcc` -DCMAKE_CXX_COMPILER=`which em++` -DCMAKE_AR=`which llvm-ar` -DCMAKE_MODULE_PATH=$EMSCRIPTEN/cmake/ -DGDCM_BUILD_DOCBOOK_MANPAGES=0 -DGDCM_DOCUMENTATION=0 -DBUILD_DOC=0 -DTEST_BIG_ENDIAN=0 -DOPJ_BIG_ENDIAN=0 -DCMAKE_STATIC_LIBRARY_SUFFIX=".bc" -DCMAKE_SHARED_LIBRARY_SUFFIX=".bc" -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
 make $*
-rm -rf emscripten.zip
-zip -r emscripten.zip bin/
+rm -rf /mobagen/bin-emscripten/emscripten.zip
+zip -r /mobagen/bin-emscripten/emscripten.zip bin/
