c41e9f105a4ab897f5e9008c9d217b6c78730bcc
Alexandre Tolstenko <tolstenko@gmail.com>
2018-03-20 23:43:57 -0300

    #41 moved static object declaration to header

--- Engine/engine/Object.cpp
+++ Engine/engine/Object.cpp
@@ -9,37 +9,6 @@ int Object::GetInstanceID(){
   return m_InstanceID;
 }
 
-void Object::Destroy(std::shared_ptr<Object>) {
-  throw NotImplementedException();
-}
-
-void Object::DestroyImmediate(std::shared_ptr<Object>) {
-  throw NotImplementedException();
-}
-
-void Object::DontDestroyOnLoad(std::shared_ptr<Object>) {
-  throw NotImplementedException();
-}
-
-template<class T>
-std::shared_ptr<T> Object::FindObjectOfType(void) {
-  throw NotImplementedException();
-  return std::shared_ptr<T>();
-}
-
-template<class T>
-std::vector<std::shared_ptr<T>> Object::FindObjectsOfType() {
-  throw NotImplementedException();
-  return std::vector<std::shared_ptr<T>>();
-}
-
-template<class T, class... _Types>
-std::shared_ptr<T> Object::Instantiate(_Types &&... _Args) {
-  auto obj = std::make_shared<T>(_Args...);
-  throw NotImplementedException();
-  return std::shared_ptr<T>();
-}
-
 bool Object::operator==(const Object &other)
 {
   // todo: improve this operator with type check, and make it tolerant to any type of object
--- Engine/engine/Object.h
+++ Engine/engine/Object.h
@@ -5,6 +5,7 @@
 #include <string>
 #include "HideFlags.h"
 #include <vector>
+#include "Error.h"
 
 class Object {
 public:
@@ -18,25 +19,40 @@ public:
   int GetInstanceID();
 
   /// Removes a gameobject, component or asset.
-  static void Destroy(std::shared_ptr<Object>);
+  static void Destroy(std::shared_ptr<Object>) {
+    throw NotImplementedException("Object Destroy");
+  }
 
   /// Destroys the object obj immediately. You are strongly recommended to use Destroy instead.
-  static void DestroyImmediate(std::shared_ptr<Object>);
+  static void DestroyImmediate(std::shared_ptr<Object>){
+    throw NotImplementedException("Object DestroyImmediate");
+  }
 
   /// Makes the object target not be destroyed automatically when loading a new scene.
-  static void DontDestroyOnLoad(std::shared_ptr<Object>);
+  static void DontDestroyOnLoad(std::shared_ptr<Object>) {
+    throw NotImplementedException("Object DontDestroyOnLoad");
+  }
 
   /// Returns the first active loaded object of Type type.
   template <class T>
-  static std::shared_ptr<T> FindObjectOfType(void);
+  static std::shared_ptr<T> FindObjectOfType() {
+    throw NotImplementedException("Object FindObjectOfType");
+    return std::shared_ptr<T>();
+  }
 
   /// Returns a list of all active loaded objects of Type type.
   template <class T>
-  static std::vector<std::shared_ptr<T>> FindObjectsOfType();
+  static std::vector<std::shared_ptr<T>> FindObjectsOfType(){
+    throw NotImplementedException("Object FindObjectsOfType");
+    return nullptr;
+  }
 
   /// Clones the object original and returns the clone.
   template <class T, class... _Types>
-  static std::shared_ptr<T> Instantiate(_Types&&... _Args);
+  static std::shared_ptr<T> Instantiate(_Types&&... _Args) {
+    auto obj = std::make_shared<T>(_Args...);
+    throw NotImplementedException("Object Instantiate");
+  }
 
   /// Compares two object references to see if they refer to the same object.
   inline bool operator == (const Object & other);
