9890acdbf0049c7004923b150767729cee5f13ea
Alexandre Tolstenko <tolstenko@gmail.com>
2018-03-22 18:07:43 -0300

    #37 emscripten restore automated build

--- scripts/emscripten-dockerbuild.sh
+++ scripts/emscripten-dockerbuild.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -dit --name emscripten -e TRAVIS_OS_NAME='linux' -e BUILD_TYPE='emscripten' -v `pwd`/../:/mobagen -w /mobagen trzeci/emscripten:sdk-incoming-64bit bash
+#docker rm --force emscripten
+docker run -dit --name emscripten -e TRAVIS_OS_NAME='linux' -e BUILD_TYPE='emscripten' -v `pwd`:/mobagen -w /mobagen trzeci/emscripten:sdk-incoming-64bit bash
 docker exec -it emscripten apt-get update
 docker exec -it emscripten rm /usr/local/bin/cmake
 docker exec -it emscripten apt-get install -y cmake wget
