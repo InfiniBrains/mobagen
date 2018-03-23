27144b899bf059e180b0183d436c4cf0e345bf3c
Alexandre Tolstenko <tolstenko@gmail.com>
2018-03-23 13:56:14 -0300

    #37: chmod deploy file

--- scripts/cmake-mingw-linux.sh
+++ scripts/cmake-mingw-linux.sh
@@ -24,5 +24,6 @@ cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DMINGW=ON -DCMAKE_C_COMPILER=`which x86_64-
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DMINGW=ON -DCMAKE_C_COMPILER=`which x86_64-w64-mingw32-gcc` -DCMAKE_CXX_COMPILER=`which x86_64-w64-mingw32-g++` -DCMAKE_AR=`which x86_64-w64-mingw32-ar` -DGDCM_BUILD_DOCBOOK_MANPAGES=0 -DGDCM_DOCUMENTATION=0 -DBUILD_DOC=0 -DTEST_BIG_ENDIAN=0 -DOPJ_BIG_ENDIAN=0 -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DTHREADSAFE=ON -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_RC_COMPILER=`which x86_64-w64-mingw32-windres` -DDLLTOOL=`which x86_64-w64-mingw32-dlltool` -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DDIRECTX=0 -DVIDEO_OPENGL=1 -DVIDEO_OPENGLES=0 -DBUILD_SHARED_LIBS=OFF -DCMAKE_EXE_LINKER_FLAGS=" -static -static-libgcc -static-libstdc++" ../
 make $*
 make $*
-rm -rf $DIR/../win64mingw.zip
-zip -r $DIR/../win64mingw.zip bin/*
+rm -rf /mobagen/win64mingw.zip
+zip -r /mobagen/win64mingw.zip bin/*
+chmod -R 777 /mobagen/
