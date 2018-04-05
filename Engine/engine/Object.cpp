fa5a665a969b7134d011375b472b7d102d785636
Alexandre Tolstenko <tolstenko@gmail.com>
2018-04-05 17:46:14 -0300

    #41: Moved functions from object.h to .cpp

--- Engine/engine/Object.cpp
+++ Engine/engine/Object.cpp
@@ -6,19 +6,19 @@
 #include "Error.h"
 
 uint64_t Object::GetInstanceID(){
-  return _id;
+  return _oid;
 }
 
 bool Object::operator==(const Object &other)
 {
   // todo: improve this operator with type check, and make it tolerant to any type of object
-  return _id == other._id;
+  return _oid == other._oid;
 }
 
 bool Object::operator!=(const Object &other)
 {
   // todo: improve this operator with type check, and make it tolerant to any type of object
-  return _id != other._id;
+  return _oid != other._oid;
 }
 
 Object::Object() : _oid(++_counter_) {}
@@ -26,4 +26,35 @@ Object::Object() : _oid(++_counter_) {}
 Object::~Object() = default;
 
 uint64_t Object::_counter_ = 0;
-std::unordered_multimap<size_t, std::unique_ptr<Object>> Object::objects;
+
+void Object::Destroy (std::shared_ptr<Object>) {
+  throw NotImplementedException("Object Destroy");
+}
+
+void Object::DestroyImmediate (std::shared_ptr<Object>) {
+  throw NotImplementedException("Object DestroyImmediate");
+}
+
+void Object::DontDestroyOnLoad (std::shared_ptr<Object>) {
+  throw NotImplementedException("Object DontDestroyOnLoad");
+}
+
+template<class T>
+std::shared_ptr<T> Object::FindObjectOfType () {
+  throw NotImplementedException("Object FindObjectOfType");
+  return std::shared_ptr<T>();
+}
+
+template<typename T>
+std::vector<std::shared_ptr<T>> Object::FindObjectsOfType () {
+  throw NotImplementedException("Object FindObjectsOfType");
+  return nullptr;
+}
+
+template<class T, class... _Types>
+std::shared_ptr<T> Object::Instantiate (_Types &&... _Args) {
+  auto obj = std::make_shared<T>(_Args...);
+  throw NotImplementedException("Object Instantiate");
+}
+
+std::unordered_multimap<std::type_index, std::shared_ptr<Object>> Object::objects;
--- Engine/engine/Object.h
+++ Engine/engine/Object.h
@@ -17,52 +17,37 @@ public:
   Object();
   virtual ~Object();
 
-  /// Should the object be hidden, saved with the scene or modifiable by the user?
+  /// \brief Should the object be hidden, saved with the scene or modifiable by the user?
   HideFlags hideFlags = HideFlags::None;
 
-  /// Returns the instance id of the object.
+  /// \brief Returns the instance id of the object.
   uint64_t GetInstanceID();
 
-  /// Removes a gameobject, component or asset.
-  static void Destroy(std::shared_ptr<Object>) {
-    throw NotImplementedException("Object Destroy");
-  }
+  /// \brief Removes a gameobject, component or asset.
+  static void Destroy(std::shared_ptr<Object>);
 
-  /// Destroys the object obj immediately. You are strongly recommended to use Destroy instead.
-  static void DestroyImmediate(std::shared_ptr<Object>){
-    throw NotImplementedException("Object DestroyImmediate");
-  }
+  /// \brief Destroys the object obj immediately. You are strongly recommended to use Destroy instead.
+  static void DestroyImmediate(std::shared_ptr<Object>);
 
-  /// Makes the object target not be destroyed automatically when loading a new scene.
-  static void DontDestroyOnLoad(std::shared_ptr<Object>) {
-    throw NotImplementedException("Object DontDestroyOnLoad");
-  }
+  /// \brief Makes the object target not be destroyed automatically when loading a new scene.
+  static void DontDestroyOnLoad(std::shared_ptr<Object>);
 
-  /// Returns the first active loaded object of Type type.
+  /// \brief Returns the first active loaded object of Type type.
   template <class T>
-  static std::shared_ptr<T> FindObjectOfType() {
-    throw NotImplementedException("Object FindObjectOfType");
-    return std::shared_ptr<T>();
-  }
+  static std::shared_ptr<T> FindObjectOfType();
 
-  /// Returns a list of all active loaded objects of Type type.
-  template <class T>
-  static std::vector<std::shared_ptr<T>> FindObjectsOfType(){
-    throw NotImplementedException("Object FindObjectsOfType");
-    return nullptr;
-  }
+  /// \brief Returns a list of all active loaded objects of Type type.
+  template <typename T>
+  static std::vector<std::shared_ptr<T>> FindObjectsOfType();
 
-  /// Clones the object original and returns the clone.
+  /// \brief Clones the object original and returns the clone.
   template <class T, class... _Types>
-  static std::shared_ptr<T> Instantiate(_Types&&... _Args) {
-    auto obj = std::make_shared<T>(_Args...);
-    throw NotImplementedException("Object Instantiate");
-  }
+  static std::shared_ptr<T> Instantiate(_Types&&... _Args);
 
-  /// Compares two object references to see if they refer to the same object.
+  /// \brief Compares two object references to see if they refer to the same object.
   inline bool operator == (const Object & other);
 
-  /// Compares if two objects refer to a different object.
+  /// \brief Compares if two objects refer to a different object.
   inline bool operator != (const Object & other);
 
 private:
@@ -70,8 +55,6 @@ private:
   uint64_t _oid;
   static uint64_t _counter_;
 
-  // todo: create class id
-
-  static std::unordered_multimap<size_t, std::unique_ptr<Object>> objects;
+  static std::unordered_multimap<std::type_index, std::shared_ptr<Object>> objects;
 };
 
