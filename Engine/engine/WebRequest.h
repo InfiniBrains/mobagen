813e74c8aff12135389c270dea09a82829f786f3
Alexandre Tolstenko <tolstenko@gmail.com>
2018-05-17 23:32:47 -0300

    #55 post binary data using cpr

--- Engine/engine/WebRequest.h
+++ Engine/engine/WebRequest.h
@@ -2,17 +2,73 @@
 
 #ifndef EMSCRIPTEN
 #include <cpr/cpr.h>
-#include <string>
 #endif
+#include <string>
+
+enum class WebErrorCode {
+  OK = 0,
+  CONNECTION_FAILURE,
+  EMPTY_RESPONSE,
+  HOST_RESOLUTION_FAILURE,
+  INTERNAL_ERROR,
+  INVALID_URL_FORMAT,
+  NETWORK_RECEIVE_ERROR,
+  NETWORK_SEND_FAILURE,
+  OPERATION_TIMEDOUT,
+  PROXY_RESOLUTION_FAILURE,
+  SSL_CONNECT_ERROR,
+  SSL_LOCAL_CERTIFICATE_ERROR,
+  SSL_REMOTE_CERTIFICATE_ERROR,
+  SSL_CACERT_ERROR,
+  GENERIC_SSL_ERROR,
+  UNSUPPORTED_PROTOCOL,
+  UNKNOWN_ERROR = 1000,
+};
+
+class WebResponse{
+public:
+  int status_code;
+  std::string text;
+  std::map<std::string, std::string> header;
+  std::string url;
+  double elapsed;
+  std::map<std::string, std::string> cookies;
+  WebErrorCode error;
+};
 
 class WebRequest {
 
 public:
   WebRequest();
+  void SetUrl(const std::string& url);
+  void AddHeader(const std::string& key, const std::string& value);
+  void SetAuth(const std::string &username, const std::string& password);
+  void SetDigest(const std::string& username, const std::string& password);
+  void SetBody(const std::string& body);
+  void AddParameter(const std::string& key, const std::string& value);
+  void AddMultiPart(const std::string& name, const std::string& value, const std::string& content_type = {});
+  void AddMultiPart(const std::string& name, const std::int32_t& value, const std::string& content_type = {});
+  void AddMultiPart(const std::string& name, const std::string& filename, const std::string& data, const std::string& content_type);
+  void SetCookies(const std::string& key, const std::string& value);
+  void AddPayload(const std::string& key, const std::string& value);
+
+
+  WebResponse Post();
+  void Get();
 
 private:
-  std::string url;
 
+#ifndef EMSCRIPTEN
+  cpr::Session m_session;
 
+  cpr::Cookies m_cookies;
+  cpr::Header m_header;
+  cpr::Parameters m_parameters;
+  cpr::Multipart m_multipart = cpr::Multipart(std::initializer_list<cpr::Part>());
+  cpr::Payload m_payload = cpr::Payload(std::initializer_list<cpr::Pair>());
+  WebResponse m_response;
+#else
+
+#endif
 };
 
--- Engine/uploader/EditorGUI.cpp
+++ Engine/uploader/EditorGUI.cpp
@@ -10,6 +10,7 @@
 #include "components/MeshRenderer.hpp"
 #include <Plane.hpp>
 #include <string>
+#include <WebRequest.h>
 #include "Material.hpp"
 //#include <imguifilesystem.h>
 #include "Logger.hpp"
@@ -64,6 +65,21 @@ EditorGUI::EditorGUI()
   windowFactor.y = getEngine()->getWindow()->getHeight()/2000.0f;
   windowFactor.x = getEngine()->getWindow()->getWidth()/2000.0f;
 
+  WebRequest request;
+
+  std::string data;
+  data.push_back('d');
+  data.push_back('\0');
+  data.push_back('a');
+  data.push_back('t');
+  data.push_back('\0');
+
+  log_info("%d",data.size());
+  log_info("%d",data.size());
+  request.SetUrl("http://httpbin.org/post");
+  request.AddMultiPart("field","filename.txt",data,"text/plain");
+  auto resp =request.Post();
+  log_info("%s",resp.text.data());
 }
 
 
