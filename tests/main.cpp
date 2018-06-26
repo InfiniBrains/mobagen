dfa958d26e7d7d40d00f346ac5f6aaa602bf6671
Alexandre Tolstenko <tolstenko@gmail.com>
2018-06-25 21:29:54 -0300

    Testing automated test. #1 try

--- tests/main.cpp
+++ tests/main.cpp
@@ -34,18 +34,12 @@ void DocEditor::update(double delta)
 
 void DocEditor::init(GLManager *glManager)
 {
-  auto cam = new GameObject("MainCamera");
-  cam->AddComponent<OrthoCamera>(getEngine()->getWindow()->getWidth() / (float)getEngine()->getWindow()->getHeight(), 512, 0.01f, 10000.0f);
+  auto cam = std::make_shared<Entity>();
+  cam->addComponent<OrthoCamera>(getEngine()->getWindow()->getWidth() / (float)getEngine()->getWindow()->getHeight(), 512, 0.01f, 10000.0f);
+  addToScene(cam);
 
-//  cam->getTransform()->setPosition(glm::vec3(0, 512, 0));
-//  cam->getTransform()->setScale(glm::vec3(1, 1, 1));
-//  cam->getTransform()->setRotation(glm::quat(0, 0, 0.707, 0.707));
-//  cam->addComponent<DirectionalLight>(glm::vec3(1,1,1), 0.5);
-//  addToScene(cam);
-
-  //auto primary_camera = cam->getComponent<OrthoCamera>();
-  std::shared_ptr<OrthoCamera> o(cam->GetComponent<OrthoCamera>());
-  getEngine()->getGLManager()->setActiveCamera(o);
+  auto primary_camera = cam->getComponent<OrthoCamera>();
+  getEngine()->getGLManager()->setActiveCamera(primary_camera);
 }
 
 int main(int argc, char *argv[]) {
