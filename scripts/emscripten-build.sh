a089aaae34d85f8b00e590f78086b757c699170f
Alexandre Tolstenko <tolstenko@gmail.com>
2018-06-29 14:57:01 -0300

    fixing emscripten build

--- scripts/emscripten-build.sh
+++ scripts/emscripten-build.sh
@@ -9,8 +9,8 @@ source ./emsdk_env.sh
 cd $DIR/../
 
 cmake -Bbuild-emscripten -H. -DGDCM_BUILD_DOCBOOK_MANPAGES=0 -DGDCM_DOCUMENTATION=0 -DBUILD_DOC=0 -DTEST_BIG_ENDIAN=0 -DOPJ_BIG_ENDIAN=0 -DCMAKE_STATIC_LIBRARY_SUFFIX=".bc" -DCMAKE_SHARED_LIBRARY_SUFFIX=".bc" -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/emscripten-cxx11.cmake
-camke --build build-emscripten
-camke --build build-emscripten
+cmake --build build-emscripten
+cmake --build build-emscripten
 
 rm -rf $DIR/../emscripten.zip
 zip -r $DIR/../emscripten.zip $DIR/../build-emscripten/bin/*
