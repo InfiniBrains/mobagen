0dbaff5b7db20fe4367848c7a6ad403a848f7213
Alexandre Tolstenko <tolstenko@gmail.com>
2018-06-29 22:45:36 -0300

    Trying to fix android build

--- scripts/cmake-android.sh
+++ scripts/cmake-android.sh
@@ -6,7 +6,9 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-android
 cd $DIR/../bin-android
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=22 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DCMAKE_TOOLCHAIN_FILE=$DIR/../cmake/toolchains/android.toolchain.cmake
+#cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_SYSTEM_VERSION=24 -DANDROID_NATIVE_API_LEVEL=24 -DCMAKE_TOOLCHAIN_FILE=/usr/local/share/android-ndk/build/cmake/android.toolchain.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r17-api-24-arm64-v8a-clang-libcxx14.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r17-api-24-arm64-v8a-clang-libcxx14.cmake
 make $*
 make android-build
 
