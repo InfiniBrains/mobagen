822d8b5caae3ca3630dd1cdec59d5ead274a2bb5
Alexandre Tolstenko <tolstenko@gmail.com>
2018-09-28 16:59:42 -0300

    temporarily disabling curl

--- Engine/WebRequest.cpp
+++ Engine/WebRequest.cpp
@@ -3,56 +3,72 @@
 namespace mobagen {
   WebRequest::WebRequest() {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
 
+#endif
 #else
 #endif
   }
 
   void WebRequest::AddHeader(const std::string &key, const std::string &value) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_header[key] = value;
+#endif
 #else
 #endif
   }
 
   void WebRequest::SetAuth(const std::string &username, const std::string &password) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_session.SetAuth(cpr::Authentication(username, password));
+#endif
 #else
 #endif
   }
 
   void WebRequest::SetUrl(const std::string &url) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_session.SetUrl(url);
+#endif
 #else
 #endif
   }
 
   void WebRequest::SetBody(const std::string &body) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_session.SetBody(cpr::Body(body));
+#endif
 #else
 #endif
   }
 
   void WebRequest::AddParameter(const std::string &key, const std::string &value) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_parameters.AddParameter(cpr::Parameter(key, value));
+#endif
 #else
 #endif
   }
 
   void WebRequest::AddMultiPart(const std::string &name, const std::string &value, const std::string &content_type) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_multipart.parts.emplace_back(cpr::Part(name, value, content_type));
+#endif
 #else
 #endif
   }
 
   void WebRequest::AddMultiPart(const std::string &name, const std::int32_t &value, const std::string &content_type) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_multipart.parts.emplace_back(cpr::Part(name, value, content_type));
+#endif
 #else
 #endif
   }
@@ -60,34 +76,43 @@ namespace mobagen {
   void WebRequest::AddMultiPart(const std::string &name, const std::string &filename, const std::string &data,
                                 const std::string &content_type) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_multipart.parts.emplace_back(cpr::Part(name, cpr::Buffer(data.begin(), data.end(), filename), content_type));
+#endif
 #else
 #endif
   }
 
   void WebRequest::SetCookies(const std::string &key, const std::string &value) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_cookies[key] = value;
+#endif
 #else
 #endif
   }
 
   void WebRequest::SetDigest(const std::string &username, const std::string &password) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_session.SetDigest(cpr::Digest(username, password));
+#endif
 #else
 #endif
   }
 
   void WebRequest::AddPayload(const std::string &key, const std::string &value) {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     m_payload.AddPair(cpr::Pair(key, value));
+#endif
 #else
 #endif
   }
 
   WebResponse WebRequest::Post() {
 #ifndef EMSCRIPTEN
+#ifdef USE_CURL
     if (m_cookies.GetEncoded().length() != 0)
       m_session.SetCookies(m_cookies);
 
@@ -114,6 +139,7 @@ namespace mobagen {
     m_response.text = resp.text;
     m_response.url = resp.url;
     return m_response;
+#endif
 #else
     return WebResponse();
 #endif
