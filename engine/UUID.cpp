2a887df56abf7103233506ee5881ff873ae27b54
Alexandre Tolstenko <tolstenko@gmail.com>
2018-10-12 14:29:06 -0300

    Closes #67

---
+++ engine/UUID.cpp
@@ -0,0 +1,29 @@
+#include "UUID.hpp"
+
+#include <atomic>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+
+UUID UUID::Generate() {
+	UUID id;
+
+	static std::atomic_uint64_t cnt{ 0 };
+	++cnt;
+
+	id.count = cnt;
+
+	auto now = std::chrono::high_resolution_clock::now();
+	id.time = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
+
+	return id;
+}
+
+std::string UUID::toString()
+{
+	std::stringstream ss;
+	ss << std::setw(16) << std::setfill('0') << std::hex << this->time;
+	ss << std::setw(16) << std::setfill('0') << std::hex << this->count;
+
+	return ss.str();
+}
--- engine/UUID.hpp
+++ engine/UUID.hpp
@@ -1,25 +1,13 @@
 #pragma once
-#include <atomic>
 #include <string>
-#include <chrono>
-#include <iomanip>
-#include <sstream>
 
 struct UUID {
+private:
+	uint64_t time;
+	uint64_t count;
 public:
-	static std::string UUIDGenerator();
+	static UUID Generate();
+	std::string toString();
+	bool operator==(const UUID& other) { return this->count == other.count && this->time==other.time; }
 };
 
-std::string UUID::UUIDGenerator() {
-	static std::atomic_uint64_t uid{ 0 };
-	++uid;
-
-	auto now = std::chrono::high_resolution_clock::now();
-	uint64_t nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
-	
-	std::stringstream ss;
-	ss << std::setw(16) << std::setfill('0') << std::hex << nanos;
-	ss << std::setw(16) << std::setfill('0') << std::hex << uid;
-
-	return ss.str();
-}
