6cb95ed4fb465acac7d211b5e1c5de79fa187749
Alexandre Tolstenko <tolstenko@gmail.com>
2018-12-19 09:42:55 -0200

    curl p7zip dependencies

--- scripts/mingw-install-toolkit.sh
+++ scripts/mingw-install-toolkit.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-pacman -Sy --noconfirm base-devel mingw-w64-x86_64-toolchain git subversion mercurial mingw-w64-x86_64-cmake
+echo "--------------------------------------------------------------"
+echo "Installing toolkit"
+echo "--------------------------------------------------------------"
+pacman -Sy --noconfirm base-devel mingw-w64-x86_64-toolchain git subversion mercurial mingw-w64-x86_64-cmake wget curl p7zip
+wget http://sourceforge.net/projects/codeblocks/files/Binaries/Nightlies/2018/CB_20181008_rev11499_win64.7z
