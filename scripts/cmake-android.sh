e3ebefc2972b8d97a0a07481381ebedc61b2ebf4
Alexandre Tolstenko <tolstenko@gmail.com>
2019-03-05 22:51:57 -0300

    android build... again

--- scripts/cmake-android.sh
+++ scripts/cmake-android.sh
@@ -6,12 +6,12 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-android
 cd $DIR/../bin-android
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=22 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DCMAKE_TOOLCHAIN_FILE=$DIR/../cmake/toolchains/android.toolchain.cmake
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=22 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DCMAKE_TOOLCHAIN_FILE=$DIR/../cmake/toolchains/android.toolchain.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r18b-api-24-arm64-v8a-clang-libcxx11.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r18b-api-24-arm64-v8a-clang-libcxx11.cmake
 make $*
 make android-build
 
 STATE=$(adb get-state || exit 0)
 if [ "$STATE" == "device" ]; then
 	make android-install android-start
-fi
+fi;
