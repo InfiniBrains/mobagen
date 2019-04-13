b73c789b4a69fb87ffc0986626d7b8ad6ff32904
Alexandre Tolstenko <tolstenko@gmail.com>
2019-04-13 15:37:38 -0300

    fixing emsdk path

--- scripts/cmake-emscripten.sh
+++ scripts/cmake-emscripten.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-emscripten
 mkdir -p $DIR/../bin-emscripten/bin/
 
-source ../dependencies/emsdk/emsdk_env.sh
+source $DIR/../dependencies/emsdk/emsdk_env.sh
 
 cd $DIR/../bin-emscripten
 cmake -DCMAKE_C_ABI_COMPILED=ON -DCMAKE_CXX_ABI_COMPILED=ON -DCMAKE_CROSSCOMPILING=ON -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/emscripten-cxx11.cmake -DCMAKE_MODULE_PATH=$EMSCRIPTEN/cmake/ -DGDCM_BUILD_DOCBOOK_MANPAGES=0 -DGDCM_DOCUMENTATION=0 -DBUILD_DOC=0 -DTEST_BIG_ENDIAN=0 -DOPJ_BIG_ENDIAN=0 -DCMAKE_STATIC_LIBRARY_SUFFIX=".bc" -DCMAKE_SHARED_LIBRARY_SUFFIX=".bc" -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
