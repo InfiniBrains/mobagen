264691ffcc2c593ae4ef09af8243b326cc6d30f2
Alexandre Tolstenko <tolstenko@gmail.com>
2019-04-20 10:45:20 -0300

    improved build emscripten

--- scripts/emscripten-build.sh
+++ scripts/emscripten-build.sh
@@ -6,5 +6,5 @@ source $DIR/../dependencies/emsdk/emsdk_env.sh
 
 cmake --build $DIR/../bin-emscripten
 
-rm -rf $DIR/../bin-emscripten/emscripten.zip
-zip -r $DIR/../emscripten.zip bin/*
+rm -rf $DIR/../emscripten.zip
+zip -r $DIR/../emscripten.zip $DIR/../bin-emscripten/bin/*
