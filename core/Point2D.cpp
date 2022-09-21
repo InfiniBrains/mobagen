b117ab013e203a86fe05f35dca64c69957eb19f2
Alexandre Tolstenko <tolstenko@gmail.com>
2022-09-21 10:57:14 -0400

    fix: point2d equality operation

--- core/Point2D.cpp
+++ core/Point2D.cpp
@@ -1,11 +1,11 @@
 #include "Point2D.h"
 
-bool Point2D::operator==(const Point2D &rhs) const {
-    return this->y==rhs.y && this->x==rhs.x;
+bool Point2D::operator==(const Point2D& rhs) const {
+    return y == rhs.y && x==rhs.x;
 }
 
-bool Point2D::operator!=(const Point2D &rhs) const {
-  return this->x == rhs.x && this->y==rhs.y;
+bool Point2D::operator!=(const Point2D& rhs) const {
+  return x != rhs.x || y!=rhs.y;
 }
 
 Point2D &Point2D::operator=(const Point2D &rhs) {
