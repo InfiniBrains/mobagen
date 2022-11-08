abc9fdefd1b6839bb63c14024cc3ad2dead72fc5
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2022-11-08 13:55:02 -0500

    Added + operator for Point2D and defined constants for up, down, left, and right

--- core/Point2D.cpp
+++ core/Point2D.cpp
@@ -1,21 +1,33 @@
 #include "Point2D.h"
 
+
+const Point2D Point2D::UP = Point2D(0, -1);
+const Point2D Point2D::DOWN = Point2D(0, 1);
+const Point2D Point2D::LEFT = Point2D(-1, 0);
+const Point2D Point2D::RIGHT = Point2D(1, 0);
+
+
 bool Point2D::operator==(const Point2D& rhs) const {
-    return y == rhs.y && x==rhs.x;
+	return y == rhs.y && x == rhs.x;
 }
 
 bool Point2D::operator!=(const Point2D& rhs) const {
-  return x != rhs.x || y!=rhs.y;
+	return x != rhs.x || y != rhs.y;
 }
 
-Point2D &Point2D::operator=(const Point2D &rhs) {
-  //Check for self-assignment
-  if (this == &rhs)
-    return *this;
-  x = rhs.x;
-  y = rhs.y;
-  return *this;
+Point2D& Point2D::operator=(const Point2D& rhs) {
+	//Check for self-assignment
+	if (this == &rhs)
+		return *this;
+	x = rhs.x;
+	y = rhs.y;
+	return *this;
 }
+
+Point2D Point2D::operator+(const Point2D& rhs) const {
+	return { x + rhs.x, y + rhs.y };
+}
+
 Point2D Point2D::operator-(const Point2D& rhs) const {
-  return {x - rhs.x, y - rhs.y};
+	return { x - rhs.x, y - rhs.y };
 }
