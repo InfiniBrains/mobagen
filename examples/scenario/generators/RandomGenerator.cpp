61eac0b3ea76eae958b325a8f55e987a467f5bf2
Alexandre Tolstenko <tolstenko@gmail.com>
2022-11-08 11:47:22 -0500

    feat: better noise example

--- examples/scenario/generators/RandomGenerator.cpp
+++ examples/scenario/generators/RandomGenerator.cpp
@@ -5,16 +5,16 @@
 // do not use this one to your assignment. this is my sample generator
 std::vector<Color32> RandomScenarioGenerator::Generate(int sideSize) {
   std::vector<Color32> colors;
-  // create your own function for noise generation
-//  siv::BasicPerlinNoise<float> noise;
-//  noise.reseed(1337);
-  Noise noise(1337, 1024,0,255);
+//   create your own function for noise generation
+  siv::BasicPerlinNoise<float> noise;
+  noise.reseed(1337);
+//  Noise noise(1337, 1024,0,255);
   for(int l=0; l<sideSize; l++){
     for(int c=0;c<sideSize; c++){
-//      float rgb = abs(noise.noise2D(c/50.0,l/50.0)*255);
-//      colors.emplace_back(rgb, rgb, rgb);
-      double color = noise.noise(c/50.0,l/50.0);
-      colors.emplace_back(color,color,color);
+      float rgb = abs(noise.noise2D(c/50.0,l/50.0)*255);
+      colors.emplace_back(rgb, rgb, rgb);
+//      double color = noise.noise(c/50.0,l/50.0);
+//      colors.emplace_back(color,color,color);
     }
   }
   std::cout<<colors.size() << std::endl;
