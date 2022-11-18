c1058af889dfd277af93aff16785bf3eca66ee20
Alexandre Tolstenko <tolstenko@gmail.com>
2022-11-18 11:56:17 -0500

    feat: fast noise

--- examples/scenario/generators/RandomGenerator.cpp
+++ examples/scenario/generators/RandomGenerator.cpp
@@ -1,16 +1,23 @@
 #include "RandomGenerator.h"
+#include "../FastNoiseLite.h"
 #include "../PerlinNoise.hpp"
 #include <iostream>
 // do not use this one to your assignment. this is my sample generator
 std::vector<Color32> RandomScenarioGenerator::Generate(int sideSize, float displacement) {
   std::vector<Color32> colors;
 //   create your own function for noise generation
-  siv::BasicPerlinNoise<float> noise;
-  noise.reseed(1337);
+  FastNoiseLite base;
+  base.SetFractalOctaves(3);
+  base.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2S);
+  FastNoiseLite cellular;
+  cellular.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
+  cellular.SetFractalOctaves(3);
   for(int l=0; l<sideSize; l++){
     for(int c=0;c<sideSize; c++){
-      float rgb = abs(noise.octave3D(c/50.0,l/50.0, displacement, 2)*255);
-      colors.emplace_back(rgb, rgb, rgb);
+      float c1 = ((base.GetNoise((float)c,(float)l, displacement*50) +1)/2) *255;
+      float c2 = ((cellular.GetNoise((float)c,(float)l, displacement*50) +1)/2) *255;
+      auto avg = (c1+c2)/2;
+      colors.emplace_back(avg, avg, avg);
     }
   }
   std::cout<<colors.size() << std::endl;
