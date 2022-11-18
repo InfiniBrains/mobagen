f35ec978f9b79b06c74d33ba33d0cf34298941a0
Alexandre Tolstenko <tolstenko@gmail.com>
2022-11-18 09:08:31 -0500

    feat: emscripten install

--- scripts/emscripten-install.sh
+++ scripts/emscripten-install.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo $DIR
 
+git clone https://github.com/emscripten-core/emsdk.git $DIR/../external/emsdk/
+
 $DIR/../external/emsdk/emsdk install latest
 $DIR/../external/emsdk/emsdk activate latest
 
