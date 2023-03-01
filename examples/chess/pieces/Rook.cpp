8471bbd8a177aa4f3462f8f087cd110edac83409
Alexandre Tolstenko <tolstenko@gmail.com>
2023-03-01 07:02:03 -0500

    rename tower to rook

--- examples/chess/pieces/Rook.cpp
+++ examples/chess/pieces/Rook.cpp
@@ -2,4 +2,4 @@
 // Created by tolstenko on 2/28/23.
 //
 
-#include "Tower.h"
+#include "Rook.h"
--- examples/chess/pieces/Rook.h
+++ examples/chess/pieces/Rook.h
@@ -2,7 +2,7 @@
 #define CHESS_TOWER_H
 
 #include "../WorldState.h"
-struct Tower: public PieceBase {
+struct Rook : public PieceBase {
   vector<Point2D> PossibleMoves(WorldState* world, const Point2D& origin) override;
   vector<WorldState> GenerateStates(WorldState* world, const Point2D& origin) override;
 };
