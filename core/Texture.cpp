44af7dded1cac311d4ebdbdfbe8e0a6811ab3574
Alexandre Tolstenko <tolstenko@gmail.com>
2023-03-03 13:09:27 -0500

    feat: fix shared pointers

--- core/Texture.cpp
+++ core/Texture.cpp
@@ -1,11 +1,18 @@
 #include "Texture.h"
 #include "SDL_image.h"
+#include "SDL_surface.h"
 
 std::shared_ptr<Texture> Texture::LoadSVGFromString(SDL_Renderer* renderer, const std::string& svgtxt) {
   auto tex = std::make_shared<Texture>();
 
   SDL_RWops *rw = SDL_RWFromConstMem(svgtxt.c_str(), svgtxt.size());
-  tex->surface = std::shared_ptr<SDL_Surface>(IMG_Load_RW(rw, 1));
-  tex->texture = std::shared_ptr<SDL_Texture>(SDL_CreateTextureFromSurface(renderer, tex->surface.get()));
+  //todo: check if it is correct
+  tex->surface = IMG_Load_RW(rw, 1);
+  tex->texture = SDL_CreateTextureFromSurface(renderer, tex->surface);
   return tex;
+}
+Texture::~Texture() {
+    SDL_DestroyTexture(texture);
+    // todo: destroy surface!
+//    SDL_DestroySurface(surface);
 }
--- core/Texture.h
+++ core/Texture.h
@@ -3,12 +3,15 @@
 #include "SDL_render.h"
 #include <memory>
 #include <string>
+
+
 class Texture {
  private:
-  std::shared_ptr<SDL_Surface> surface;
-  std::shared_ptr<SDL_Texture> texture;
+  SDL_Surface* surface = nullptr;
+  SDL_Texture* texture = nullptr;
  public:
   Texture();
+  ~Texture();
   // todo: hide or wrap this sdl_renderer type
   static std::shared_ptr<Texture> LoadSVGFromString(SDL_Renderer * renderer, const std::string& svgtxt);
 };
