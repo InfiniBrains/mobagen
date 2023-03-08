26e3915b92bca40ffd71dce2340addaf469025f9
Alexandre Tolstenko <tolstenko@gmail.com>
2023-03-07 22:01:42 -0500

    fix: disable test coverage on windows ci builds

--- scripts/emscripten-build.sh
+++ scripts/emscripten-build.sh
@@ -24,6 +24,6 @@ cmake --version
 
 emcmake cmake --version
 
-emcmake cmake -DCMAKE_C_ABI_COMPILED=ON -DCMAKE_CXX_ABI_COMPILED=ON -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -H. -Bbin-emscripten -DBUILD_EDITOR=ON
+emcmake cmake -DCMAKE_C_ABI_COMPILED=ON -DCMAKE_CXX_ABI_COMPILED=ON -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -H. -Bbin-emscripten
 
 cmake --build bin-emscripten/ --parallel
--- scripts/windows-build.bat
+++ scripts/windows-build.bat
@@ -1,2 +1,2 @@
-cmake -H. -Bbuild -G "Visual Studio 16 2019" -T ClangCL -DCMAKE_BUILD_TYPE=MinSizeRel -DBUILD_EDITOR=ON
+cmake -H. -Bbuild -G "Visual Studio 16 2019" -T ClangCL -DCMAKE_BUILD_TYPE=MinSizeRel
 cmake --build build --config MinSizeRel
